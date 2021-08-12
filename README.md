0da9dfab571f4c4612e29fe78a252cc58eda06a8
Glenn Elliron Clark <Elliron.Clark@gmail.com>
2021-08-12 14:00:38 -0700

    basic frame is done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rpg_fighter_tutorial
+
+## overview
+- Following a tutorial with branches and notes on making a rpg fighter in pygame
+
+## Credits
+- https://coderslegacy.com/python/pygame-rpg-building-the-base/
---
+++ game.py
@@ -0,0 +1,25 @@
+import pygame, sys
+from config import *
+
+class Game(pygame.sprite.Sprite):
+    pass
+
+# # creates display for game surface
+# displaysurface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+# # changes window title from default to the new title
+# pygame.display.set_caption("Glenns RPG Fighter Game Practice")
+
+# while True:
+#     for event in pygame.event.get():
+#         # will run when the close window button is clicked
+#         if event.type == QUIT:
+#             pygame.quit()
+#             sys.exit()
+
+#         # for events that occur upon clicking the left mouse button
+#         if event.type == pygame.MOUSEBUTTONDOWN:
+#             pass
+
+#         # event handling for a range of different key presses
+#         if event.type == pygame.KEYDOWN:
+#             pass
